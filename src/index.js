b8949090dd83cf50d5a6ff52c207473026e28cd7
Richard M <richardmaichs276@gmail.com>
2020-11-02 01:42:09 -0500

    updated scripts to deploy

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+ReactDOM.render(    
+<BrowserRouter basename={process.env.PUBLIC_URL}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+
+  </React.StrictMode>
+  </BrowserRouter>
+  ,
+
   document.getElementById('root')
 );
 
